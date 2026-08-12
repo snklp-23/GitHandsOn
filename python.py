a2217ed8f9cede4d6ff25a5e03506d76eac0d861
snklp <snklp.kumar23@gmail.com>
2026-08-12 17:47:22 +0530

    added the correct code

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ for i in range(10):
     print("hello i times " , i)
 
 print( " printing the table of 2" )
-    for i in range(10):
-        print(  "2" * i =  , 2 * i)
+for i in range(10):
+    print(  "2" ,"*" , i+1 , "=" , 2 * (i+1))
         
     
