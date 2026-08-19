6e854e1c564d2eaea73b5afb6d6d2c35cde55497
snklp <snklp.kumar23@gmail.com>
2026-08-19 15:42:47 +0530

    added the workflowfolder and python code

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
-def add(num1,num2)
-    return num1+num2;
+def add(num1,num2):
+    return num1+num2
 
-def add():
-    assert add(4,5) ==3
+def test_add():
+    assert add(4,5) ==9
     assert add(4,-8) ==-4
 
